53e607e68a4841f4c7ef5d2c68a763802cbe8440
projectcs2103t <projectcs2103t@gmail.com>
2015-06-16 14:33:46 +0800

    working version

---
+++ js/main.js
@@ -0,0 +1,120 @@
+/* jshint jquery:true, devel:true */
+
+var container,stats;
+var camera, scene, renderer;
+
+init();
+animate();
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function init(){
+    
+    //renderer
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+    renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor( 0xf0f0f0 ); //background
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight ); 
+    container.appendChild( renderer.domElement );
+    
+    //stats
+    stats = new Stats();
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.top = '0px';
+    container.appendChild( stats.domElement );
+    
+    //camera
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.001, 1000 ); 
+    camera.position.z = 1;
+    
+    //lighting
+    var ambient = new THREE.AmbientLight( 0x666666 );
+    var light = new THREE.DirectionalLight( 0xdfebff, 1.75 );
+    light.position.set( 50, 200, 100 );
+    light.position.multiplyScalar( 1.3 );
+    light.castShadow = false;
+    //light.shadowCameraVisible = true;
+    light.shadowMapWidth = 1024;
+    light.shadowMapHeight = 1024;
+    var d = 300;
+    light.shadowCameraLeft = -d;
+    light.shadowCameraRight = d;
+    light.shadowCameraTop = d;
+    light.shadowCameraBottom = -d;
+    light.shadowCameraFar = 1000;
+    light.shadowDarkness = 0.5;
+    
+    //scene
+    scene = new THREE.Scene();
+    scene.add( ambient );
+    scene.add( light );
+    
+    //material
+    var clothTexture = THREE.ImageUtils.loadTexture( 'pic.jpg' );
+    var material = new THREE.MeshPhongMaterial( 
+        { 
+            alphaTest: 0.5, 
+            color: 0xffffff, 
+            specular: 0x030303, //0x333333
+            emissive: 0x111111, 
+            shiness: 50, 
+            map: clothTexture, 
+            shading: THREE.SmoothShading,
+            wireframe: false,
+            side: THREE.DoubleSide 
+        }
+    ); 
+    
+    //models
+    var manager = new THREE.LoadingManager();
+    manager.onProgress = function ( item, loaded, total ) {
+        console.log( item, loaded, total );
+    }; 
+
+    var onProgress = function ( xhr ) {
+        if ( xhr.lengthComputable ) {
+            var percentComplete = xhr.loaded / xhr.total * 100;
+            console.log( Math.round(percentComplete, 2) + '% downloaded' );
+        }
+    };
+
+    var onError = function ( xhr ) {
+    };
+
+    var loader = new THREE.OBJLoader( manager );
+    loader.load( 'bun_zipper.obj', function ( object ) {
+        object.traverse( function (child) {  
+            if ( child instanceof THREE.Mesh ) {  
+                child.material = material;  
+                //child.material.needsUpdate = true;  
+            }  
+        });
+        
+        scene.add( object );
+    }, onProgress, onError );
+    
+    //var geometry = new THREE.BoxGeometry( 0.1, 0.1, 0.1 );  
+    //var cube = new THREE.Mesh( geometry, material ); 
+    //cube.material.needsUpdate = true;
+    //scene.add( cube ); 
+    
+    
+    
+    window.addEventListener( 'resize', onWindowResize, false );
+    controls = new THREE.OrbitControls( camera );
+
+}
+function animate() {
+    requestAnimationFrame( animate );
+    render();
+    stats.update();
+}
---
+++ js/render.js
@@ -0,0 +1,6 @@
+function render() { 
+    //requestAnimationFrame( render ); 
+    renderer.render( scene, camera ); 
+    //cube.rotation.x += 0.01; 
+    //cube.rotation.y += 0.01;
+} 
